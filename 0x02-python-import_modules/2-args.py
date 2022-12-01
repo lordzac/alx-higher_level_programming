7a277e33b0dd471dbb602ed452559b7f8cb74648
lordzac <balogunzacchaeus@gmail.com>
2022-12-01 15:19:23 -0800

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(i))
 
-        if i >= 1:
-            i = 0
-            for arg in sys.argv:
-                if i != 0:
-                    print("{}: {}".format(i, arg))
-                i += 1
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
